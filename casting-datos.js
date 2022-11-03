f5d30b7c5e0f6f03285897845fb6d3c4a270d7ee
Jose Vilchez <jhtgus971454@gmail.com>
2022-11-03 01:03:18 -0500

    0113JV

---
+++ casting-datos.js
@@ -0,0 +1,17 @@
+// CONVERSION DE ENTERO A STRING 
+
+/* let dato = 15; 
+console.log( typeof(dato));
+
+dato = String(dato);
+console.log( typeof(dato)); */
+
+let dato = 15; 
+console.log( typeof(dato));
+
+dato = toString;
+console.log( typeof(dato));
+
+
+
+
